f4117018e95919459bfc2a29429dd0401bbc118c
takidilmi <angeldilmi@gmail.com>
2023-08-06 15:47:53 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+/* function findMatching(drivers, name) {    
+    return drivers.filter((driver) => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter((driver) => driver.toLowerCase().startsWith(name.toLowerCase()))
+}
+
+function matchName(drivers, name) {
+    return drivers.filter((driver) => driver.name.toLowerCase() === name.toLowerCase());
+} */
+/////////////////////////////////////////////////////////////////////////////////////////
+
+const findMatching = (drivers, name) =>
+  drivers.filter((driver) => driver.toLowerCase() === name.toLowerCase());
+
+const fuzzyMatch = (drivers, name) =>
+  drivers.filter((driver) => driver.toLowerCase().startsWith(name.toLowerCase()));
+
+const matchName = (drivers, name) =>
+  drivers.filter((driver) => driver.name.toLowerCase() === name.toLowerCase());
